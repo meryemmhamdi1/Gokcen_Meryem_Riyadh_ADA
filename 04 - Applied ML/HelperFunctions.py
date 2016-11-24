2547078db64013f9aab2f651fce638ee43e028e0
meryemmhamdi1 <meryemmhamdi1@gmail.com>
2016-11-24 23:58:07 +0100

    [HelperFunctions] Added Descriptions for helper functions

--- 04 - Applied ML/HelperFunctions.py
+++ 04 - Applied ML/HelperFunctions.py
@@ -3,11 +3,6 @@ import numpy as np
 from sklearn.cross_validation import cross_val_score
 from sklearn.model_selection import cross_val_predict, learning_curve
 
-
-##### Functıons used ın the fırst part #####
-
-
-
 # Create Categories for Binary Classification
 def binaryColor(x):
     if x < 0.5: 
@@ -27,6 +22,8 @@ def multiColor(x):
         return 3
     
 def weight_sample(labels):
+    """ We calculate the percentages for each class and add it to the sample_weights list"""
+    
     weight_class = labels.value_counts()/len(labels)
     
     sample_weights = []
@@ -35,7 +32,14 @@ def weight_sample(labels):
         
     return np.array(sample_weights)
 
-def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title(title)
     plt.colorbar()
@@ -48,8 +52,9 @@ def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix'
     
 
 def plot_learning_curve(estimator,X,Y,cv=20):
+    """ Plotting the learning curve (accuracy score) for training and testing data by trying for different sizes of data"""
     plt.figure()
-    plt.xlabel("the sıze of the data")
+    plt.xlabel("Training examples")
     plt.ylabel("Score")
     train_sizes, train_scores, test_scores = learning_curve(estimator, X, Y, cv=cv, train_sizes=np.linspace(0.2,1,20))
     train_scores_mean = np.mean(train_scores, axis=1)
@@ -58,64 +63,13 @@ def plot_learning_curve(estimator,X,Y,cv=20):
     test_scores_std = np.std(test_scores, axis=1)
     plt.grid()
 
-    plt.fill_between(train_sizes, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, alpha=0.1, color="b")
-    plt.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.1, color="r")
-    plt.plot(train_sizes, train_scores_mean, 'o-', color="b", label="Training score")
-    plt.plot(train_sizes, test_scores_mean, 'o-', color="r", label="Cross-validation score")
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                    train_scores_mean + train_scores_std, alpha=0.1, color="b")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                    test_scores_mean + test_scores_std, alpha=0.1, color="r")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="b",
+            label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="r",
+            label="Cross-validation score")
     plt.legend(loc="best")
     plt.show()
-
-
-
-
-##### Functıons used ın the second part #####
-
-def get_kmeans_result(dataframe, combination):
-    # get_dummies generates neccessary columns to convert Categorical features to numerical.
-    _df_aggregated_with_dummies = pd.get_dummies(dataframe[combination])
-    
-    labeled, score = sil_score(_df_aggregated_with_dummies)
-    
-    cluster_0 = df_skin[labeled == 0]
-    cluster_1 = df_skin[labeled == 1]
-    
-    cluster_0_blacks = len(cluster_0[cluster_0['skin'] > 0.5])
-    cluster_0_whites = len(cluster_0[cluster_0['skin'] <= 0.5])
-    
-    cluster_1_blacks = len(cluster_1[cluster_1['skin'] > 0.5])
-    cluster_1_whites = len(cluster_1[cluster_1['skin'] <= 0.5])
-    
-    return {
-        'sil_score': score,
-        'features': ','.join(list(_df_aggregated_with_dummies.columns)),
-        'cluster_0_whites': cluster_0_whites,
-        'cluster_1_whites': cluster_1_whites,
-        'cluster_0_blacks': cluster_0_blacks,
-        'cluster_1_blacks': cluster_1_blacks,
-        'cluster_0_total': cluster_0_whites + cluster_0_blacks,
-        'cluster_1_total': cluster_1_whites + cluster_1_blacks
-    }
-
-
-# We will be using this function to test our feauture configurations.
-def sil_score(df_aggr):
-    '''
-    sil_score applies KMeans on the given paremeter df_aggr (a pandas DataFrame) by using all
-    its columns as features, and returns results of clustering and silhouette score.
-    '''
-    scale = StandardScaler()
-    df_aggr_scaled = scale.fit_transform(df_aggr.as_matrix())
-    kmeans_model = KMeans(n_clusters=2, random_state=0, init='k-means++', n_jobs=1).fit(df_aggr_scaled)
-    labels = kmeans_model.labels_
-    return labels, silhouette_score(df_aggr_scaled, labels)
-
-
-def get_tuple(combination,result):
-    return (
-        ','.join(combination),
-        result['cluster_0_blacks']/result['cluster_0_total'],
-        result['cluster_1_blacks']/result['cluster_1_total'],
-        result['sil_score'],
-        result['cluster_0_total'],
-        result['cluster_1_total']
-    )
